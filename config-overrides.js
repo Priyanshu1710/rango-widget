9f46bf45bff94645c8c340041961f9af1b646992
RanGojo <rangojo@rango.exchange>
2023-04-27 21:43:00 +0300

    workaround for fixing loader problem

--- config-overrides.js
+++ config-overrides.js
@@ -1,32 +1,53 @@
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
 
 module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        assets: false,
-        fs: false,
-        tls: false,
-        net: false,
-        buffer: false,
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        path: require.resolve("path-browserify"),
-        stream: require.resolve("stream-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-        os: require.resolve("os-browserify/browser"),
-        zlib: require.resolve("browserify-zlib"),
+  config.resolve.fallback = {
+    assets: false,
+    fs: false,
+    tls: false,
+    net: false,
+    buffer: false,
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    path: require.resolve("path-browserify"),
+    stream: require.resolve("stream-browserify"),
+    crypto: require.resolve("crypto-browserify"),
+    os: require.resolve("os-browserify/browser"),
+    zlib: require.resolve("browserify-zlib"),
+  };
 
-    };
+  config.resolve.extensions = [...config.resolve.extensions, ".js", ".ts"];
+  config.plugins = [
+    ...config.plugins,
+    new MiniCssExtractPlugin(),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ];
+  config.module.rules = [
+    {
+      test: /\.m?ts$|\.tsx?$/,
+      // exclude: /node_modules/,
+      use: {
+        loader: "ts-loader",
+        options: {
+          onlyCompileBundledFiles: true,
+        },
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader"],
+    },
+    {
+      test: /\.(woff|woff2|ttf|eot|png|jpg|svg|gif)$/i,
+      use: ["file-loader"],
+    },
+  ];
 
-    config.resolve.extensions = [...config.resolve.extensions, ".js", ".ts"];
-    config.plugins = [
-        ...config.plugins,
-        new webpack.ProvidePlugin({
-            Buffer: ["buffer", "Buffer"],
-        }),
-        new webpack.ProvidePlugin({
-            Process: ["process/buffer"],
-        }),
-    ];
-    return config;
+  return config;
 };
